370f44670cf64fcc00eefe595479d7fe72109b58
정명진 <thinkjin99@gmail.com>
2024-08-07 00:10:18 +0900

    [feat] Add response factory method
    뷰함수의 리턴값을  응답 객체의 팩토리 메서드를 통해 생성하도록 수정했습니다.
    이제 뷰 함수를 통해 생성되는 예외를 제외한 모든 리턴 값은 해당 메서드를 통해 생성됩니다.

--- blog/response/validator.py
+++ blog/response/validator.py
@@ -5,3 +5,14 @@ class ResponseValidator(BaseModel):
     message: str
     status: int = Field(default=200)
     data: dict | None = Field(default=None)
+
+    def __init__(self, status: int, message: str, data: dict | None = None):
+        super().__init__(status=status, message=message, data=data)
+
+    @classmethod
+    def created(cls, data):
+        return cls(201, "created", data)
+
+    @classmethod
+    def success(cls, message: str = "success", data: dict | None = None):
+        return cls(200, message, data)
