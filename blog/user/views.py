5acc440cc3c3db2ad628c82c1082b951d9e2f994
정명진 <thinkjin99@gmail.com>
2024-08-21 06:31:01 +0900

    [feat] Add User view
    로그인, 회원가입, 엑세스 토큰 발급에 대한 뷰를 작성했습니다.
    모든 뷰가 데이터베이스에 레코드를 추가하는 작업 만을 수행하므로
    createApiView를 상속 받아 구현했습니다.

--- blog/user/views.py
+++ blog/user/views.py
@@ -4,7 +4,7 @@ from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework.request import Request
 
-from serializers.user_serializer import (
+from .serializers import (
     LoginSerializer,
     RegisterSerializer,
     RefreshTokenSerializer,
