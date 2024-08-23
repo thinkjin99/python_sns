e9d87c3f30c4e79ee55c27b400abe4174a81b317
정명진 <thinkjin99@gmail.com>
2024-08-23 11:57:13 +0900

    [Fix] Can change author_id in post API
    포스트 api에서 마음대로 작성자를 수정할 수 있던 문제를 수정했습니다. 이제 포스트에서 작성자를 변경할 수 없습니다.

--- blog/post/serializers.py
+++ blog/post/serializers.py
@@ -14,5 +14,12 @@ class PostSerializer(serializers.ModelSerializer):
     title = serializers.CharField(max_length=50, required=True)
     content = serializers.CharField(max_length=500, required=True)
     author = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all(), default=CurrentUserDefault()
+        read_only=True
+        # queryset=User.objects.all(), default=CurrentUserDefault()
     )
+
+    def create(self, validated_data):
+        validated_data["author"] = self.context[
+            "request"
+        ].user  # author값은 시스템에서 자동 초기화 해야한다. read_only는 save에 포함되지 않는다.
+        return super().create(validated_data)
