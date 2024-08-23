a19b952511cd96a3e94654da0251039367a728a0
정명진 <thinkjin99@gmail.com>
2024-08-23 11:02:03 +0900

    [Fix] Change post_id or autor_id in comment API
    코멘트 API에서 post_ID나 author_id를 입력해 전달하면
    수정할 수 있던 문제를 해결횄습니다.
    
    각 메서드 별로 시리얼라이져I를 구분했습니다.
    
    author 필드의 경우 read_only로 설정해 시리얼라이져에서 DB저장 직전에 시스템적으로 입력하게 수정했습니다.

--- blog/comment/views.py
+++ blog/comment/views.py
@@ -5,8 +5,11 @@ from rest_framework.exceptions import ParseError
 
 
 from .models import Comment
-from .serializers import CommentSerializer
-from .filters import CommentFilter
+from .serializers import (
+    CommentGetSerializer,
+    CommentCreateSerializer,
+    CommentUpdateSerializer,
+)
 from auth.authenicator import JWTAuthenicator
 from permissions.author import IsAuthorOrReadOnly
 
@@ -15,11 +18,23 @@ class CommentViewSet(viewsets.ModelViewSet):
     # 해당 뷰에서는 코멘트의 추가,수정,삭제,개별 조회만 가능해야 한다.
     authentication_classes = [JWTAuthenicator]
     permission_classes = [IsAuthorOrReadOnly]
-    serializer_class = CommentSerializer
+    serializer_class = CommentGetSerializer
     queryset = Comment.objects.all()
 
-    @action(detail=True, methods=["GET"], url_path="posts/comments")
-    def post_list(self, *args, **kwargs):
+    # def get_serializer_class(self):
+    #     if self.action == "create":
+    #         return CommentCreateSerializer
+    #     elif self.action == "update":
+    #         return CommentUpdateSerializer
+    #     return self.serializer_class
+
+    @action(
+        detail=True,
+        methods=["GET"],
+        url_path="posts/comments",
+        serializer_class=CommentGetSerializer,
+    )
+    def list_post_comments(self, *args, **kwargs):
         self.lookup_field = "post_id"
         queryset = self.filter_queryset(self.get_queryset())
         serializer = self.get_serializer(
@@ -27,8 +42,16 @@ class CommentViewSet(viewsets.ModelViewSet):
         )  # 쿼리셋을 집어넣었는데 유효성 검사를 할 필요는 없다, 쿼리셋의 직렬화를 위해서만 사용
         return Response(serializer.data)
 
-    def list(self, request, *args, **kwargs):
-        """
-        기본 `list` 액션을 오버라이드하여 모든 `GET` 요청을 거부할 수 있습니다.
-        """
-        raise ParseError(detail="Missing Prameter comment_id")
+    def create(self, request, *args, **kwargs):
+        kwargs.setdefault("context", self.get_serializer_context())
+        serializer = CommentCreateSerializer(data=request.data, **kwargs)
+        serializer.is_valid(raise_exception=True)
+
+        self.perform_create(serializer)
+        return Response(data=serializer.data, status=201)
+
+    # def list(self, request, *args, **kwargs):
+    #     """
+    #     기본 `list` 액션을 오버라이드하여 모든 `GET` 요청을 거부할 수 있습니다.
+    #     """
+    #     raise ParseError(detail="Missing Prameter comment_id")
