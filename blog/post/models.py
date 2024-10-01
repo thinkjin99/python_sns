2e3f546521c24850e2b2f8dedaacc5174a8dccf9
정명진 <thinkjin99@gmail.com>
2024-10-01 20:15:12 +0900

    [feat] Add validate time and game in post model
    포스트 모델 내부에 유효시간과 게임 이름을 저장하는 공간을 추가했습니다.

--- blog/post/models.py
+++ blog/post/models.py
@@ -1,13 +1,24 @@
+import datetime
 from django.db import models
+
 from user.models import User
 
 
+def get_one_hour_from_now():
+    return datetime.datetime.now() + datetime.timedelta(hours=1)
+
+
 # Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=50)
+
+
 class Post(models.Model):
-    id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=50, null=False, blank=False)
     content = models.CharField(max_length=500, null=False)
     author = models.ForeignKey(User, related_name="post", on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.DO_NOTHING, blank=False, null=False)
+    valid_time = models.DateTimeField(default=get_one_hour_from_now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- blog/post/views.py
+++ blog/post/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.generics import GenericAPIView, ListAPIView
 
 
 from .models import Post
@@ -11,7 +12,7 @@ from permissions.author import IsAuthorOrReadOnly
 
 class PostPagination(PageNumberPagination):
     page_size = 5
-    # page_query_param = "page_num"
+    # page_query_param = "page_num" 
 
 
 class PostViewSet(viewsets.ModelViewSet):
