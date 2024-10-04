2bfb7c043e64df321ace114bdb43b298148c4459
정명진 <thinkjin99@gmail.com>
2024-10-04 14:52:22 +0900

    Add only author permission
    작성자만 호출 가능케 하는 권한 클래스를 작성했습니다.

--- blog/permissions/author.py
+++ blog/permissions/author.py
@@ -9,8 +9,17 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     # 작성자에 한해 레코드에 대한 수정/삭제 허용
     def has_object_permission(self, request, view, obj):
         # 조회 요청(GET, HEAD, OPTIONS) 에 대해 인증여부 상관없이 허용
-        if request.method in ("PUT", "DELETE"):
-            return obj.author == request.user
-        else:
+        if request.method in permissions.SAFE_METHODS:
             return True
+        else:
+            return obj.author == request.user
+
         # PUT, DELETE 요청에 대해 작성자일 경우 요청 허용
+
+
+class OnlyAuthor(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
